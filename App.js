826ad9080edb7faa4c58d2a25cd85e029375a6c0
aupous <aupous@Aupiouss-MacBook-Pro.local>
2017-12-14 00:26:21 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+
+export default class App extends React.Component {
+  render() {
+    return "./Home";
+  }
+}
